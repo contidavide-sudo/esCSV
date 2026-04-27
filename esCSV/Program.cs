f9ae6a5983030a11f94014e40381e79f723d2015
contidavide-sudo <conti.davide@franchettisalviani.net>
2026-04-27 08:58:28 +0200

    Aggiungere i file di progetto.

---
+++ esCSV/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace esCSV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ID = new List<string>();
+            List<string> nome = new List<string>();
+            List<string> categoria = new List<string>();
+            List<string> prezzo = new List<string>();
+            List<string> disponibilita = new List<string>();
+
+            string[] arrPar;
+            int sommaP = 0;
+            float media;
+
+            using (StreamReader sr = new StreamReader("prodotti_abbigliamento.csv"))
+            {
+                string riga = sr.ReadLine(); 
+
+                while (riga != null) 
+                {
+                    riga = sr.ReadLine();
+
+                    arrPar = riga.Split(',');
+
+                    ID.Add(arrPar[0]);
+                    nome.Add(arrPar[1]);
+                    categoria.Add(arrPar[2]);
+                    prezzo.Add(arrPar[3]);
+
+                    sommaP = sommaP + Convert.ToInt32(arrPar[3]);
+
+                    disponibilita.Add(arrPar[4]);
+
+                }
+            }
+
+
+
+
+        }
+    }
+}
